f4a64bf7fba6c72bc2b05e46c9a4ded1648e5eff
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2024-02-01 19:18:11 +0800

    Avoid global variable declare them within a module
    add change: rename variables so they make sense make it even
      better to reason why do you need them in the first place.

--- src/js-modules/image-slider.js
+++ src/js-modules/image-slider.js
@@ -1,44 +1,26 @@
-// * functions down here depends on this two variables  and i think it's not really a good idea due to global pollution
+const elementGetter = function getImageUsingNextAndPreviousFunction() {
+  const images = [];
+  let imagesIndex = 0;
 
-const elements = [];
-let elementsIndex = 0;
+  const getNextImage = () => {
+    if (imagesIndex >= images.length - 1) {
+      imagesIndex = 0;
+      return imagesIndex;
+    }
+    imagesIndex += 1;
+    return imagesIndex;
+  };
 
-function next(index, array) {
-  if (index >= array.length - 1) {
-    return 0;
-  }
-  elementsIndex += 1;
-  return elementsIndex;
-}
+  const getPreviousImage = () => {
+    if (imagesIndex === 0) {
+      imagesIndex = images.length - 1;
+      return imagesIndex;
+    }
+    imagesIndex -= 1;
+    return imagesIndex;
+  };
 
-function previous() {
-  if (elementsIndex === 0) {
-    elementsIndex = elements.length - 1;
-    return elementsIndex;
-  }
-  elementsIndex -= 1;
-  return elementsIndex;
-}
+  return { getNextImage, getPreviousImage, images };
+};
 
-// * code above is tightly coupled, think of how you can come up with a loose coupled technique, aysus
-
-/**
- * * function 'hideAllArrayItem' below is used to add a classList to an elements
- * * function 'showCurrentArrayItem' below is used to toggle classList of an element
- */
-
-function hideAllArrayItem(array, classListVal) {
-  return array.forEach((item) => item.classList.add(classListVal));
-}
-
-function showCurrentArrayItem(array, currentIndex, classListVal) {
-  return array.at(currentIndex).classList.toggle(classListVal);
-}
-
-export { next, previous, hideAllArrayItem, showCurrentArrayItem };
-
-// todo: make this code clean
-
-// ? how to use this?
-// * for next button use the next function together with hideAllArrayItem and showCurrentArrayitem
-// * for previous button use the previous function together with hideAllArrayItem and showCurrentArrayitem
+export default elementGetter;
