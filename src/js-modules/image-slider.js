2a02feadecc772cd3a89cb57655e85a3af3e1351
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2024-02-03 15:48:34 +0800

    Explain how functions can be used for image slider

--- src/js-modules/image-slider.js
+++ src/js-modules/image-slider.js
@@ -2,6 +2,7 @@ const elementSetter = function addMultipleElements({
   imagesToAdd,
   arrayOfImage,
 }) {
+  // you want to add all images to an array
   const addMultipleImages = () =>
     imagesToAdd.forEach((image) => {
       arrayOfImage.push(image);
@@ -10,9 +11,12 @@ const elementSetter = function addMultipleElements({
 };
 
 const arrayIndexPosition = () => {
+  // used to calculate the array length and later be used to manipulated images when using the image position function
   const arrayOfImages = [];
+  // inialized default value
   let index = 0;
 
+  // every time forward btn is clicked, increase index value by 1 and is restricted by the lengths of an array
   const nextIndex = () => {
     if (index >= arrayOfImages.length - 1) {
       index = 0;
@@ -22,6 +26,7 @@ const arrayIndexPosition = () => {
     return index;
   };
 
+  // every time back btn is clicked, decrease index value by 1 and is restricted by the lengths of an array
   const previousIndex = () => {
     if (index === 0) {
       index = arrayOfImages.length - 1;
@@ -31,7 +36,7 @@ const arrayIndexPosition = () => {
     return index;
   };
 
-  return { nextIndex, previousIndex };
+  return { nextIndex, previousIndex, arrayOfImages };
 };
 
 const imagePosition =
@@ -39,6 +44,7 @@ const imagePosition =
     imageWidth,
     index,
   }) {
+    // use this result value to translate horizontally all selected images to one place
     const positionGetter = imageWidth * index;
 
     return {
